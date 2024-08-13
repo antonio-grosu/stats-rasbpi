5705c5490de4ad8ac27ca00a3963a5ad3d8d4623
Grosu Antonio <codevs@Grosus-MacBook-Air.local>
2024-08-14 02:43:59 +0300

    made changes to code

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ const app = express();
 const os = require("os");
 const { execSync } = require("child_process");
 
+//⚠️ Functions to format Data
+// Convert bytes to gigabytes
+const bytesToGB = (bytes) => (bytes / 1024 ** 3).toFixed(2);
+// Convert uptime from seconds to hours, minutes, and seconds
+const secondsToHMS = (seconds) => {
+  const hrs = Math.floor(seconds / 3600);
+  const mins = Math.floor((seconds % 3600) / 60);
+  const secs = Math.floor(seconds % 60);
+  return `${hrs}h ${mins}m ${secs}s`;
+};
+
+//GET METHOD
+
 app.get("/", (req, res) => {
+  const cpuInfo = os.cpus();
+  const totalMem = os.totalmem();
+  const formattedTotalMem = bytesToGB(totalMem);
+  const freeMem = os.freemem();
+  const formattedFreeMem = bytesToGB(freeMem);
+  const upTime = os.uptime();
+  const formattedUptime = secondsToHMS(upTime);
+  const temperature = execSync("vcgencmd measure_temp").toString().trim();
   const stats = {
-    cpuInfo: os.cpus(),
-    totalMem: os.totalmem(),
-    freeMem: os.freemem(),
-    upTime: os.uptime(),
-    temperature: execSync("vcgencmd measure_temp").toString().trim(),
+    cpu: cpuInfo,
+    freeMemory: `${formattedFreeMem} GB / ${formattedTotalMem} GB`,
+    upTime: formattedUptime,
+    temp: `${temperature} °C`,
   };
   res.json(stats);
 });
